3a2e8a95314ccf671614bd0b99553c31447fab7b
Daniel Flaws <dflaws1@gmail.com>
2017-05-25 20:47:49 -0400

    Add initial Book tests

---
+++ HandAndFoot.Core.Test/BookTests.cs
@@ -0,0 +1,31 @@
+﻿using HandAndFoot.Core;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HandAndFoot.Core.Test
+{
+    [TestClass]
+    public class BookTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Empty()
+        {
+            var test = new Book(new Card[]
+            {
+
+            });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Two()
+        {
+            var test = new Book(new Card[]
+            {
+                new Card(Rank.ACE, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.CLUBS)
+            });
+        }
+    }
+}
