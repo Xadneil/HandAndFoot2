cc0d4767cd92d54b21746cb15cd0451db7c2f865
Daniel Flaws <dflaws1@gmail.com>
2017-05-28 16:08:22 -0400

    Add client stub with card images

---
+++ HandAndFoot.Client/CardControl.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HandAndFoot.Client
+{
+    partial class CardControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCard = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlCard
+            // 
+            this.pnlCard.BackColor = System.Drawing.Color.White;
+            this.pnlCard.BackgroundImage = global::HandAndFoot.Client.Properties.Resources._10_of_clubs;
+            this.pnlCard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlCard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCard.Location = new System.Drawing.Point(0, 0);
+            this.pnlCard.Name = "pnlCard";
+            this.pnlCard.Size = new System.Drawing.Size(222, 323);
+            this.pnlCard.TabIndex = 0;
+            // 
+            // CardControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.pnlCard);
+            this.Name = "CardControl";
+            this.Size = new System.Drawing.Size(222, 323);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCard;
+    }
+}
---
+++ HandAndFoot.Client/CardControl.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HandAndFoot.Core;
+using HandAndFoot.Client.Properties;
+
+namespace HandAndFoot.Client
+{
+    public partial class CardControl : UserControl
+    {
+        Card Card;
+
+        public CardControl(Card card)
+        {
+            Card = card;
+            InitializeComponent();
+            pnlCard.BackgroundImage = GetImageResource(Card);
+        }
+
+        private Bitmap GetImageResource(Card card)
+        {
+            switch (card.Suit)
+            {
+                case Suit.HEARTS:
+                    switch (card.Rank)
+                    {
+                        case Rank.ACE:
+                            return Resources.ace_of_hearts;
+                        case Rank.TWO:
+                            return Resources._2_of_hearts;
+                        case Rank.THREE:
+                            return Resources._3_of_hearts;
+                        case Rank.FOUR:
+                            return Resources._4_of_hearts;
+                        case Rank.FIVE:
+                            return Resources._5_of_hearts;
+                        case Rank.SIX:
+                            return Resources._6_of_hearts;
+                        case Rank.SEVEN:
+                            return Resources._7_of_hearts;
+                        case Rank.EIGHT:
+                            return Resources._8_of_hearts;
+                        case Rank.NINE:
+                            return Resources._9_of_hearts;
+                        case Rank.TEN:
+                            return Resources._9_of_hearts;
+                        case Rank.JACK:
+                            return Resources.jack_of_hearts;
+                        case Rank.QUEEN:
+                            return Resources.queen_of_hearts;
+                        case Rank.KING:
+                            return Resources.king_of_hearts;
+                        case Rank.JOKER:
+                            return Resources.joker;
+                    }
+                    break;
+                case Suit.DIAMONDS:
+                    switch (card.Rank)
+                    {
+                        case Rank.ACE:
+                            return Resources.ace_of_diamonds;
+                        case Rank.TWO:
+                            return Resources._2_of_diamonds;
+                        case Rank.THREE:
+                            return Resources._3_of_diamonds;
+                        case Rank.FOUR:
+                            return Resources._4_of_diamonds;
+                        case Rank.FIVE:
+                            return Resources._5_of_diamonds;
+                        case Rank.SIX:
+                            return Resources._6_of_diamonds;
+                        case Rank.SEVEN:
+                            return Resources._7_of_diamonds;
+                        case Rank.EIGHT:
+                            return Resources._8_of_diamonds;
+                        case Rank.NINE:
+                            return Resources._9_of_diamonds;
+                        case Rank.TEN:
+                            return Resources._9_of_diamonds;
+                        case Rank.JACK:
+                            return Resources.jack_of_diamonds;
+                        case Rank.QUEEN:
+                            return Resources.queen_of_diamonds;
+                        case Rank.KING:
+                            return Resources.king_of_diamonds;
+                    }
+                    break;
+                case Suit.CLUBS:
+                    switch (card.Rank)
+                    {
+                        case Rank.ACE:
+                            return Resources.ace_of_clubs;
+                        case Rank.TWO:
+                            return Resources._2_of_clubs;
+                        case Rank.THREE:
+                            return Resources._3_of_clubs;
+                        case Rank.FOUR:
+                            return Resources._4_of_clubs;
+                        case Rank.FIVE:
+                            return Resources._5_of_clubs;
+                        case Rank.SIX:
+                            return Resources._6_of_clubs;
+                        case Rank.SEVEN:
+                            return Resources._7_of_clubs;
+                        case Rank.EIGHT:
+                            return Resources._8_of_clubs;
+                        case Rank.NINE:
+                            return Resources._9_of_clubs;
+                        case Rank.TEN:
+                            return Resources._9_of_clubs;
+                        case Rank.JACK:
+                            return Resources.jack_of_clubs;
+                        case Rank.QUEEN:
+                            return Resources.queen_of_clubs;
+                        case Rank.KING:
+                            return Resources.king_of_clubs;
+                    }
+                    break;
+                case Suit.SPADES:
+                    switch (card.Rank)
+                    {
+                        case Rank.ACE:
+                            return Resources.ace_of_spades;
+                        case Rank.TWO:
+                            return Resources._2_of_spades;
+                        case Rank.THREE:
+                            return Resources._3_of_spades;
+                        case Rank.FOUR:
+                            return Resources._4_of_spades;
+                        case Rank.FIVE:
+                            return Resources._5_of_spades;
+                        case Rank.SIX:
+                            return Resources._6_of_spades;
+                        case Rank.SEVEN:
+                            return Resources._7_of_spades;
+                        case Rank.EIGHT:
+                            return Resources._8_of_spades;
+                        case Rank.NINE:
+                            return Resources._9_of_spades;
+                        case Rank.TEN:
+                            return Resources._9_of_spades;
+                        case Rank.JACK:
+                            return Resources.jack_of_spades;
+                        case Rank.QUEEN:
+                            return Resources.queen_of_spades;
+                        case Rank.KING:
+                            return Resources.king_of_spades;
+                    }
+                    break;
+            }
+            throw new Exception("Invalid Card");
+        }
+    }
+}
